05b78308e3b69dbdad1d9198d939c11eb2aa20e1
AMiro <aj.miro.b@gmail.com>
2023-02-20 10:25:35 -0600

    pseudocode and other setup stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # challenge-04-coding-quiz
 Bootcamp Challenge 04: JavaScript Coding Quiz
+
+
+https://meyerweb.com/eric/tools/css/reset/index.html
+https://learntheweb.courses/topics/html-semantics-checklist/
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash#example_%E2%80%94_guess_the_number_game
---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// display start button
+// on click, record score as 0 and start timer
+// display questions and multiple choice answer buttons
+// on click, check whether answer is correct
+/* if correct:
+    stop player from being able to guess again
+    display "you got it" message
+    increase score by 1 and record new score */
+/* if incorrect:
+    stop player from being able to guess again
+    display "you suck" message
+    decrease timer by 5s */
+// when final question answered, reduce timer to 0
+// when timer 0, display final score and name entry form
+// on submit, save and display name + score and display them along with game restart button
+// on restart, reset game logic and UI (less names+scores) and return to step 1
